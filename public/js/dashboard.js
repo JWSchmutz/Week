3ab2c9418b6dc0fd2ec2e545be8569263b39c7e3
mcnamarakc <mcnamarakc@users.noreply.github.com>
2019-08-29 14:21:44 -0400

    added single item checkout button and payment screen

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -24,3 +24,28 @@
       }
     });
   });
+
+  var checkoutButton1 = document.getElementById('checkout-button-sku_FiIm0pOWGrT2Bc');
+  checkoutButton1.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      items: [{sku: 'sku_FiIm0pOWGrT2Bc', quantity: 1}],
+
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: 'https://your-website.com/success',
+      cancelUrl: 'https://your-website.com/canceled',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
